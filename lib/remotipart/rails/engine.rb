fe413b590826e1b73694706a4c83a136369f799c
Jerome Giudicelli <jayrom@gmail.com>
2016-11-29 12:04:32 -0500

    fixed loading into WA
    * because we are not using `ActionDispatch::ParamsParser` we need to use
    another middleware in order to load this gem

--- lib/remotipart/rails/engine.rb
+++ lib/remotipart/rails/engine.rb
@@ -14,13 +14,7 @@ module Remotipart
       end
 
       initializer "remotipart.include_middelware" do
-        if ::Rails.version >= '5'
-          # Rails 5 no longer instantiates ActionDispatch::ParamsParser
-          # https://github.com/rails/rails/commit/a1ced8b52ce60d0634e65aa36cb89f015f9f543d
-          config.app_middleware.use Middleware
-        else
-          config.app_middleware.insert_after ActionDispatch::ParamsParser, Middleware
-        end
+        config.app_middleware.use Middleware
       end
     end
 
